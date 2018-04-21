6a57b73a9d3b0d1c3d117e07ecfd25d59d1ee0be
Code Next <codenext@Codes-MacBook-Air-3.local>
2018-04-21 13:06:34 -0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-/* remove this comment and place your JavaScript here */ 
+/* remove this comment and place your JavaScript here */
+let position = null;
+let posture = null;
+let mentality = null;
+let pageTitle = document.querySelector('h1');
+let pageTitleText = pageTitle.innerHTML;
+let tryAgain = document.querySelector("#try-again"),
+let quizWrapper = document.querySelector("#quiz-wrapper"),
+let result = document.querySelector("#result"),
+let formSubmit = document.querySelector("#form-submit") ;
+tryAgain.addEventListener("click", resetQuiz);
+formSubmit.addEventListener("click", processResults);
+
+function processResults(){
+
+
+}
+function getPersonality(){
+let score = 0;
+if(position.id === "GK"){
+  score += 1
+}
+
+}
+function resetQuiz(){
+
+}
