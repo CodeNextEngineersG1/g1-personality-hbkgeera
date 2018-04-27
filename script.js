e1b331ff9ec303fb4e201f4779ced4fadd1074cd
Code Next <codenext@MacBook-Air.local>
2018-04-26 17:51:35 -0700

    new

--- script.js
+++ script.js
@@ -17,9 +17,60 @@ function processResults(){
 }
 function getPersonality(){
 let score = 0;
-if(position.id === "GK"){
-  score += 1
+if(position.id === "gk"){
+  score += 1;
 }
+else if(position.id === "forward"){
+  score += 2;
+}
+else if(position.id === "midfielder"){
+  score += 3;
+}
+else if(position.id === "defender"){
+  score += 4;
+}
+if(posture.id === "De-Gea"){
+  score += 1;
+}
+else if (posture.id === "Ronaldo"){
+  score += 2;
+
+}
+else if (posture.id === "Isco"){
+  score += 3;
+
+}
+else if (posture.id === "Marcelo"){
+  score += 4;
+
+}
+if(mentality.id === "Tough"){
+  score += 1;
+}
+else if(mentality.id === "scorer"){
+  score += 2;
+}
+else if(mentality.id === "play-maker"){
+  score += 3;
+}
+else if(mentality.id === "Lock-down"){
+  score += 4;
+}
+
+if(score == 10 || score == 11 || score ==12){
+  return 1;
+}
+else if(score == 6 || score == 7 || score ==8){
+  return 2;
+}
+else if(score == 3 || score == 4 || score ==5){
+  return 3;
+}
+else if(score == 8 || score == 9 ){
+  return 4;
+}
+
+
 
 }
 function resetQuiz(){
